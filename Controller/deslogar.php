70fbd8c94a7fb0ab88fd132ca9c160100f83b725
NATHAN PEREIRA CAVALCANTE <nathan.rodrigu3s@gmail.com>
2020-05-13 14:12:27 -0300

    Update Login

--- Controller/deslogar.php
+++ Controller/deslogar.php
@@ -4,6 +4,6 @@
     unset($_SESSION['idPessoa'], $_SESSION['login'], $_SESSION['nome'], $_SESSION['cargo'], $_SESSION['ativo'], $_SESSION['Img_url']);
     $_SESSION['login'] = false;
     $_SESSION['nome'] = "";
-    include_once '../View/tela_login.php';
+    header("Location: ../");
 
 ?>
